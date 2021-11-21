44a2d3e6e3e33d3fab88ec11e0147ca36fd4e279
kabweobeed <kabweobed@gmail.com>
2021-11-21 16:32:30 -0300

    criar HOME componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,12 +28,14 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 
 //componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

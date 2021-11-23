0568c525bac6aa4710aede3454ca0fac02284f77
kabweobeed <kabweobed@gmail.com>
2021-11-22 21:44:09 -0300

    criando component LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -5,6 +6,10 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
  {
    path:'',
    component: NavComponent,

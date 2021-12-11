400eb634420366d2a83cc7e60ee20bd2ef3a7dad
kabweobeed <kabweobed@gmail.com>
2021-12-10 22:32:19 -0300

    Rotas para compnent Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { LoginComponent } from './components/login/login.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
@@ -15,7 +16,8 @@ const routes: Routes = [
    component: NavComponent,
 children: [
   {path:'home', component: HomeComponent},
-  {path:'tecnicos', component:  TecnicoListComponent}
+  {path:'tecnicos', component:  TecnicoListComponent},
+  {path:'tecnicos/create', component:  TecnicoCreateComponent}
 
 ]
 }
